2de600726e8c36fb8d8af8a183f632e01e1534ab
David <yerssondavidf@gmail.com>
2025-11-06 19:16:05 -0500

    Modify atributes in entitys

--- src/main/java/com/semillero/Ibero/Finavex/model/User.java
+++ src/main/java/com/semillero/Ibero/Finavex/model/User.java
@@ -13,18 +13,31 @@ public class User {
     private Long id;
 
     @Column(name="nombre", nullable = false, length = 100)
-    private String nombre;
+    private String name;
+
+    @Column(name="Segundo Nombre", nullable = true, length = 100)
+    private String middleName;
+
+    @Column(name="Apellido", nullable = false, length = 100)
+    private String surname;
+
+    @Column(name="Segundo Apellido", nullable = true, length = 100)
+    private String secondSurname;
+
+    @Column(name="age", nullable= false, length=3)
+    private Integer age;
+
+    @Column(name="fecha_nacimiento", nullable = false, length = 8)
+    private String dateOfBirth;
+
+    @Column(name="telefono", nullable = false, length = 10)
+    private String phone;
 
     @Column(name="email", nullable = false, length = 100, unique = true)
     @Email
     private String email;
 
-    @Column(name="fecha_nacimiento", nullable = false, length = 8)
-    private String fechaNacimiento;
-
     @Column(name="password", nullable = false, length = 100)
     private String password;
 
-    @Column(name="telefono", nullable = false, length = 10)
-    private String telefono;
 }
