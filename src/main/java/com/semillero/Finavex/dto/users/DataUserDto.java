10b4e70140f4582a51d8040dc1520df33b636853
David <yerssondavidf@gmail.com>
2025-11-19 14:47:04 -0500

    Added constructor in DTO of user

--- src/main/java/com/semillero/Finavex/dto/users/DataUserDto.java
+++ src/main/java/com/semillero/Finavex/dto/users/DataUserDto.java
@@ -1,9 +1,12 @@
 package com.semillero.Finavex.dto.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class DataUser {
+@AllArgsConstructor
+public class DataUserDto {
     private String name;
+    private String surname;
     private String email;
 }
