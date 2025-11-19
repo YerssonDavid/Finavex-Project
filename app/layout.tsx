5a7bde7fa562ffb0b355c3d54fad3f403d22cad6
David <yerssondavidf@gmail.com>
2025-11-19 12:19:08 -0500

    Added layout of app in general

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
 import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Suspense } from "react"
+import type {Metadata} from "next"
+import {GeistSans} from "geist/font/sans"
+import {GeistMono} from "geist/font/mono"
+import {Analytics} from "@vercel/analytics/next"
+import {Suspense} from "react"
 import "./globals.css"
+import {UserProvider} from "../context/ContextUserData";
 
 export const metadata: Metadata = {
-  title: "Finavex - Transforma tu Futuro Financiero",
-  description:
-    "Tu aliado inteligente para gestionar tus finanzas de manera profesional, completamente gratuita y con la seguridad que mereces.",
-  generator: "v0.app",
+    title: "Finavex - Transforma tu Futuro Financiero",
+    description:
+        "Tu aliado inteligente para gestionar tus finanzas de manera profesional, completamente gratuita y con la seguridad que mereces.",
+    generator: "v0.app",
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="es" className="dark" suppressHydrationWarning>
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
-        <Suspense fallback={null}>{children}</Suspense>
-        <Analytics />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="es" className="dark" suppressHydrationWarning>
+            <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`} suppressHydrationWarning>
+                <UserProvider>
+                    <Suspense fallback={null}>
+                        {children}
+                    </Suspense>
+                    <Analytics/>
+                </UserProvider>
+            </body>
+        </html>
+    );
 }
