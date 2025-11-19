30cef7cc324b7eac595cdae99390751da67e5d2a
David <yerssondavidf@gmail.com>
2025-11-19 14:24:32 -0500

    Modify apiResponse. Added new attribute of localDate and data user

--- backend/src/main/java/com/semillero/Finavex/dto/ApiResponse.java
+++ backend/src/main/java/com/semillero/Finavex/dto/ApiResponse.java
@@ -1,12 +1,22 @@
 package com.semillero.Finavex.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApiResponse {
-    private String code;
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private Integer status;
     private String message;
     private boolean success;
+    private LocalDateTime timestamp;
+    private T data;
 }
