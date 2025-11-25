cd3898cde2d3d05c0b9d5b16122160c9a27bd5c0
David <yerssondavidf@gmail.com>
2025-11-24 19:26:57 -0500

    Merge backend changes from backend branch to main

---
+++ backend/src/main/java/com/semillero/Finavex/dto/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.semillero.Finavex.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private Long userId;
+    private String email;
+    private String name;
+}
+
