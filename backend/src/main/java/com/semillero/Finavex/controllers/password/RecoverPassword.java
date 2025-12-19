17fb890840d6e42a08108a51c40031a95b6d94c9
David <yerssondavidf@gmail.com>
2025-12-18 21:24:53 -0500

    Added controller for the management of change password

--- backend/src/main/java/com/semillero/Finavex/controllers/password/RecoverPassword.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/password/RecoverPassword.java
@@ -1,4 +1,45 @@
 package com.semillero.Finavex.controllers.password;
 
-public class RecoveryPassword {
+import com.semillero.Finavex.dto.ApiResponse;
+import com.semillero.Finavex.services.Users.ChangePassword;
+import com.semillero.Finavex.services.recoveryPassword.ConfirmationCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("recover-password")
+@RequiredArgsConstructor
+public class RecoverPassword {
+    private final ChangePassword changePassword;
+    private final ConfirmationCode confirmationCode;
+
+
+    // Documentation Swagger (Pending)
+    @PostMapping("code")
+    public ResponseEntity<?> validationCodeRecoveryPassword (@RequestParam String email, Long code){
+        return confirmationCode.comparisonCode(email, code);
+    }
+
+    @PostMapping
+    @Operation(
+            summary = "Recover password",
+            description = "Endpoint to recover user password by providing the old and new passwords",
+            method = "POST",
+            tags = {"Password Recovery"},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Old and new passwords",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class)
+                    ),
+                    required = true
+
+            )
+    )
+    public ResponseEntity<ApiResponse> recoverPassword(@RequestBody String oldPassword, String newPassword) {
+        return changePassword.changePassword(oldPassword, newPassword);
+    }
 }
