33fdcb86479960c606636a55eda80d45fb5f574b
David <yerssondavidf@gmail.com>
2025-12-18 21:25:45 -0500

    Added services for change of the password

--- backend/src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/CodeExpirationEntry.java
+++ backend/src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/CodeExpirationEntry.java
@@ -1,4 +1,22 @@
 package com.semillero.Finavex.services.emails.codeRecoverPassword;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class CodeExpirationEntry {
+    @Getter
+    private Long codeVerification;
+    private LocalDateTime createAt;
+
+    public boolean isExpired(LocalDateTime timeRegistry) {
+        LocalDateTime timeExpiration = timeRegistry.plusMinutes(10);
+        return LocalDateTime.now().isAfter(timeExpiration);
+    }
 }
