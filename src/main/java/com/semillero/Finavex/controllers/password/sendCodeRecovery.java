1f55418a62435050202e20135a361447dfa1c7f8
David <yerssondavidf@gmail.com>
2025-12-18 21:24:53 -0500

    Added controller for the management of change password

--- src/main/java/com/semillero/Finavex/controllers/password/sendCodeRecovery.java
+++ src/main/java/com/semillero/Finavex/controllers/password/sendCodeRecovery.java
@@ -1,4 +1,29 @@
 package com.semillero.Finavex.controllers.password;
 
-public class sendCodeRevory {
+import com.semillero.Finavex.dto.ResponseCodePassword;
+import com.semillero.Finavex.dto.ResponseComparisonCode;
+import com.semillero.Finavex.services.emails.codeRecoverPassword.EmailSendCode;
+import com.semillero.Finavex.services.recoveryPassword.ConfirmationCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("code-recovery")
+@RequiredArgsConstructor
+public class sendCodeRecovery {
+    private final EmailSendCode emailSendCode;
+    private final ConfirmationCode confirmationCode;
+
+    // Send code recovery password to email user
+    @PostMapping("send")
+    public ResponseEntity<?> sendCodeRecoveryForEmail (String email){
+        return emailSendCode.sendEmailCodeRecoverPassword(
+                email,
+                "Codigo de recuperación de contraseña FINAVEX",
+                "El código expirara en 10 minutos.\nEl código de recuperación es: "
+        );
+    }
 }
