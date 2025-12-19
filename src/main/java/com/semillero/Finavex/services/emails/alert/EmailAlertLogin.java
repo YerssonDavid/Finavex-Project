052eadd8344a8de98b0bde2aaeea63d3282319fc
David <yerssondavidf@gmail.com>
2025-12-18 21:25:45 -0500

    Added services for change of the password

--- src/main/java/com/semillero/Finavex/services/emails/alert/EmailAlertLogin.java
+++ src/main/java/com/semillero/Finavex/services/emails/alert/EmailAlertLogin.java
@@ -1,6 +1,5 @@
-package com.semillero.Finavex.services.emails;
+package com.semillero.Finavex.services.emails.alert;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
--- src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/EmailSendCode.java
+++ src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/EmailSendCode.java
@@ -1,8 +1,10 @@
-package com.semillero.Finavex.services.emails.senCodeRecoverPassword;
+package com.semillero.Finavex.services.emails.codeRecoverPassword;
 
 
 import com.semillero.Finavex.dto.ApiResponse;
 import com.semillero.Finavex.dto.ResponseCodePassword;
+import com.semillero.Finavex.repository.UserR;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Slf4j
@@ -19,34 +22,57 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class EmailSendCode {
     private final JavaMailSender mailSender;
+    private final UserR userR;
+
+    @Getter
+    public final java.util.concurrent.ConcurrentHashMap<String, CodeExpirationEntry> codeRecoveryPassword = new java.util.concurrent.ConcurrentHashMap<>();
 
     @Value("${spring.mail.from}")
     private String fromEmail;
 
     // Method to send recovery password email with code
-    public ResponseEntity<ResponseCodePassword> sendEmailCodeRecoverPassword(String to, String subject, String text) {
+    public ResponseEntity<?> sendEmailCodeRecoverPassword(String to, String subject, String text) {
         try {
             log.info("Intentando enviar email de recuperación de contraseña a: {} con asunto: {}", to, subject);
 
-            // Code for recovery password
-            Long code = 100000L + new Random().nextLong(900000L);
+            if(userR.existsByEmail(to)){
+                log.info("Usuario encontrado!");
 
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(fromEmail);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text + " El código de recuperación es: " + code);
+                // Generate code for recovery password
+                Long code = 100000L + new Random().nextLong(900000L);
 
-            mailSender.send(message);
+                CodeExpirationEntry codeExpirationEntry = new CodeExpirationEntry(code, LocalDateTime.now());
 
-            log.info("Email de recuperación de contraseña enviado exitosamente a: {}", to);
+                // Storage code with expiration time and Email of the user
+                codeRecoveryPassword.put(to, codeExpirationEntry);
 
-            // Build response
-            ResponseCodePassword response = ResponseCodePassword.builder()
-                    .code(code)
-                    .message("Código de recuperación enviado exitosamente.")
-                    .build();
-            return ResponseEntity.ok(response);
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setFrom(fromEmail);
+                message.setTo(to);
+                message.setSubject(subject);
+                message.setText(text + code);
+
+                mailSender.send(message);
+
+                log.info("Email de recuperación de contraseña enviado exitosamente a: {}", to);
+
+                // Build response
+                ResponseCodePassword response = ResponseCodePassword.builder()
+                        .code(code)
+                        .message("Código de recuperación enviado exitosamente.")
+                        .build();
+                return ResponseEntity.ok(response);
+            } else {
+                log.warn("El email ingresado no está registrado");
+
+                ApiResponse responseError = ApiResponse.builder()
+                        .status(400)
+                        .message("El email ingresado no está registrado!")
+                        .success(false)
+                        .timestamp(LocalDateTime.now())
+                        .build();
+                return ResponseEntity.badRequest().body(responseError);
+            }
         } catch (MailException e) {
             log.error("Error al enviar email de recuperación de contraseña a {}: {}", to, e.getMessage(), e);
             return ResponseEntity.badRequest().build();
