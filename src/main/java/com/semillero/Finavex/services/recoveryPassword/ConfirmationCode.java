73fab405c06456b4c9c5ae146f01f23cd5d6a172
David <yerssondavidf@gmail.com>
2025-12-20 18:48:25 -0500

    Modify the imports

--- src/main/java/com/semillero/Finavex/services/recoveryPassword/ConfirmationCode.java
+++ src/main/java/com/semillero/Finavex/services/recoveryPassword/ConfirmationCode.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.security.CodeSigner;
 import java.time.LocalDateTime;
