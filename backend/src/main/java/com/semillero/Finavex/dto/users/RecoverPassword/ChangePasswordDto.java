a744af1005d9c2a6e91b1fff4e79e2fc681ceda1
David <yerssondavidf@gmail.com>
2025-12-22 20:36:15 -0500

    Added DTO of changePassword

--- backend/src/main/java/com/semillero/Finavex/dto/users/RecoverPassword/ChangePasswordDto.java
+++ backend/src/main/java/com/semillero/Finavex/dto/users/RecoverPassword/ChangePasswordDto.java
@@ -1,4 +1,17 @@
 package com.semillero.Finavex.dto.users.RecoverPassword;
 
-public class ChanguePassword {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ChangePasswordDto {
+    @NotBlank(message="El email es obligatorio")
+    @Email(message="El email no es valido!")
+    private String email;
+
+    @NotBlank(message="La contraseña es obligatoria")
+    private String newPassword;
 }
