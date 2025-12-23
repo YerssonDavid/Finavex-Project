a9fed7a8eae6fd15c3b6a304cc3892e63bc82c8b
David <yerssondavidf@gmail.com>
2025-12-22 20:38:12 -0500

    Added class async email sender

--- backend/src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/AsyncEmailSender.java
+++ backend/src/main/java/com/semillero/Finavex/services/emails/codeRecoverPassword/AsyncEmailSender.java
@@ -1,4 +1,41 @@
 package com.semillero.Finavex.services.emails.codeRecoverPassword;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class AsyncEmailSender {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String fromEmail;
+
+    @Async("taskExecutor")
+    public void sendEmailToUser(String to, String subject, String text, Long code) {
+        try {
+            log.info("Thread: {}", Thread.currentThread().getName());
+
+            log.info("Usuario encontrado!");
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text + code);
+
+            mailSender.send(message);
+
+            log.info("Email de recuperación de contraseña enviado exitosamente a: {}", to);
+        } catch (MailException e) {
+            log.error("Error al enviar email a {}: {}", to, e.getMessage(), e);
+        }
+    }
 }
