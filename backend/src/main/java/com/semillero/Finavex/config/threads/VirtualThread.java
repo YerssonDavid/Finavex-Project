000ff8601e7003e4f36e5d631c13b554580eaaaf
David <yerssondavidf@gmail.com>
2025-12-29 21:11:25 -0500

    Added config of virtual threads

--- backend/src/main/java/com/semillero/Finavex/config/threads/VirtualThread.java
+++ backend/src/main/java/com/semillero/Finavex/config/threads/VirtualThread.java
@@ -1,4 +1,15 @@
 package com.semillero.Finavex.config.threads;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
 public class VirtualThread {
+
+    @Bean
+    public Executor executor(){
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
 }
