d1e4afdc8808a1b41e761a058d5335b146e41d6e
David <yerssondavidf@gmail.com>
2025-12-31 20:15:42 -0500

    Added validation in request

--- backend/src/main/java/com/semillero/Finavex/controllers/aiControllers/AIController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/aiControllers/AIController.java
@@ -3,6 +3,7 @@ package com.semillero.Finavex.controllers.aiControllers;
 import com.semillero.Finavex.dto.aiDto.RequestAI;
 import com.semillero.Finavex.dto.aiDto.ResponseAI;
 import com.semillero.Finavex.services.assistendAI.AssistendAIBot;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ public class AIController {
     private final AssistendAIBot assistendAIBot;
 
     @PostMapping("/question")
-    public ResponseEntity<ResponseAI> askQuestion(@RequestBody RequestAI question){
+    public ResponseEntity<ResponseAI> askQuestion(@Valid @RequestBody RequestAI question){
         Float temperature = 0.7f;
         Integer tokenMax = 950;
        return assistendAIBot.explain(question);
