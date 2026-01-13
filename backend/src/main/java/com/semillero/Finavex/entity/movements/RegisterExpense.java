82582da9a724f7b59c00bee65965b96e73d8ed64
David <yerssondavidf@gmail.com>
2026-01-12 20:29:59 -0500

    Added entity of registry expenses of Users

--- backend/src/main/java/com/semillero/Finavex/entity/movements/RegisterExpense.java
+++ backend/src/main/java/com/semillero/Finavex/entity/movements/RegisterExpense.java
@@ -1,4 +1,26 @@
 package com.semillero.Finavex.entity.movements;
 
+import com.semillero.Finavex.entity.User;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="Gastos")
 public class RegisterExpense {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="Valor-gasto", nullable=false)
+    private Double expenseAmount;
+
+    @Column(name="Fecha-movimiento-gasto", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name="nota-movimiento", nullable=true)
+    private String noteMovement;
+
+    @ManyToOne
+    @JoinColumn(name="id-user", nullable=false, unique = false)
+    private User user;
 }
