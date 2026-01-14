1514c45e53c937cc0ea2a817a4d10a86d610e30e
David <yerssondavidf@gmail.com>
2026-01-13 22:03:10 -0500

    change route of repository

--- backend/src/main/java/com/semillero/Finavex/repository/movementsR/SaveR.java
+++ backend/src/main/java/com/semillero/Finavex/repository/movementsR/SaveR.java
@@ -2,6 +2,33 @@ package com.semillero.Finavex.repository.movementsR;
 
 import com.semillero.Finavex.entity.movements.SaveMoney;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface moneyR extends JpaRepository<SaveMoney, Long> {
+import java.time.LocalDateTime;
+
+public interface SaveR extends JpaRepository<SaveMoney, Long> {
+
+    @Query("""
+                SELECT COALESCE(SUM(m.savedAmount), 0)
+                FROM SaveMoney m
+                WHERE m.user.id = (SELECT u.id FROM User u WHERE u.email = :email)
+                  AND m.date >= :start
+                  AND m.date < :end
+            """)
+
+    Double sumByPeriod(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("email") String email
+    );
+
+    //Exist user by Email
+    @Query("""
+            SELECT COUNT(m) > 0
+            FROM SaveMoney m
+            WHERE m.user.id = (SELECT u.id FROM User u WHERE u.email = :email)
+        """)
+    boolean existsByEmail(@Param("email") String email);
 }
+
