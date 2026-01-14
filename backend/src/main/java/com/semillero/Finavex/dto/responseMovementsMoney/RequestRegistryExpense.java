7128d3247f7609103a7da202c5ea9b39b2b2c92c
David <yerssondavidf@gmail.com>
2026-01-13 21:55:19 -0500

    Added request registry expense

--- backend/src/main/java/com/semillero/Finavex/dto/responseMovementsMoney/RequestRegistryExpense.java
+++ backend/src/main/java/com/semillero/Finavex/dto/responseMovementsMoney/RequestRegistryExpense.java
@@ -1,4 +1,22 @@
 package com.semillero.Finavex.dto.responseMovementsMoney;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestRegistryExpense {
+    private Double ExpenseAmount;
+    private String note;
+    private LocalDate date;
+
+    @NotBlank(message="El email no puede estar vacio")
+    @Email(message="El email debe tener un formato valido")
+    private String email;
 }
