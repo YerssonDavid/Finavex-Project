5e2b9d981465b507b6af03cb729014a261342185
David <yerssondavidf@gmail.com>
2026-01-13 22:06:50 -0500

    Added service of sum total of save money

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/SumTotalSaveMonth.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/SumTotalSaveMonth.java
@@ -1,4 +1,42 @@
 package com.semillero.Finavex.services.movementsS;
 
+import com.semillero.Finavex.dto.responseMovementsMoney.ResponseSumTotalSaveMonth;
+import com.semillero.Finavex.repository.movementsR.SaveR;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class SumTotalSaveMonth {
+    private final SaveR saveR;
+
+    public ResponseEntity<ResponseSumTotalSaveMonth> sumTotalSaveMonth (String email){
+        if(email == null || email.isEmpty() || !saveR.existsByEmail(email)){
+            log.error("El email no existe o no es llega!");
+            //log.warn(email);
+            return ResponseEntity.badRequest().build();
+        }
+
+        LocalDate now = LocalDate.now();
+
+        LocalDateTime start = now
+                .withDayOfMonth(1)
+                .atStartOfDay();
+
+        LocalDateTime end = start.plusMonths(1);
+
+        Double sum = saveR.sumByPeriod(start, end, email);
+
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+        String sumFormat = format.format(sum);
+
+        return ResponseEntity.ok(new ResponseSumTotalSaveMonth(sumFormat));
+    }
 }
