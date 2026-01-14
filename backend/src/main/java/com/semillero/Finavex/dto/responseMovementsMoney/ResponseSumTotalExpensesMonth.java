10ad86f3c4ae35f3cdc277ed5a57549f381c103a
David <yerssondavidf@gmail.com>
2026-01-13 22:00:15 -0500

    Added dto of response sum total Expenses user

--- backend/src/main/java/com/semillero/Finavex/dto/responseMovementsMoney/ResponseSumTotalExpensesMonth.java
+++ backend/src/main/java/com/semillero/Finavex/dto/responseMovementsMoney/ResponseSumTotalExpensesMonth.java
@@ -1,4 +1,14 @@
 package com.semillero.Finavex.dto.responseMovementsMoney;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ResponseSumTotalExpensesMonth {
+    private String totalExpensesMonth;
 }
