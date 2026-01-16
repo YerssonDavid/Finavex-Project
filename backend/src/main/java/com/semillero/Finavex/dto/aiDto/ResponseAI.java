a7beb3b9dfac945aeceec887128052c961c44f99
David <yerssondavidf@gmail.com>
2026-01-15 20:38:29 -0500

    Add validation and JavaDoc to AI response record

--- backend/src/main/java/com/semillero/Finavex/dto/aiDto/ResponseAI.java
+++ backend/src/main/java/com/semillero/Finavex/dto/aiDto/ResponseAI.java
@@ -1,17 +1,12 @@
 package com.semillero.Finavex.dto.aiDto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class ResponseAI {
-    private String response;
-    private LocalDateTime timeResponse;
+public record ResponseAI (
+        String response,
+        LocalDateTime timeResponse
+) {
 }
