34d0145243dc6531eb4c02cae25d556959e2f9b5
David <yerssondavidf@gmail.com>
2026-01-17 17:24:49 -0500

    Change annotation of component to service

--- backend/src/main/java/com/semillero/Finavex/services/jwt/TokenProvider.java
+++ backend/src/main/java/com/semillero/Finavex/services/jwt/TokenProvider.java
@@ -8,14 +8,14 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Service
 @Slf4j
 public class TokenProvider {
 
