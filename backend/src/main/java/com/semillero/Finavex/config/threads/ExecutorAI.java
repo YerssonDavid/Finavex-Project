f13aab082b7b8606fe6ae591771217b86d275bb4
David <yerssondavidf@gmail.com>
2026-01-17 17:22:19 -0500

    Add thread pool executor configuration for AI tasks

---
+++ backend/src/main/java/com/semillero/Finavex/config/threads/ExecutorAI.java
@@ -0,0 +1,15 @@
+package com.semillero.Finavex.config.threads;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ExecutorAI {
+    @Bean
+    public ExecutorService executorAITask () {
+        return Executors.newFixedThreadPool(5);
+    }
+}
---
+++ backend/src/main/java/com/semillero/Finavex/config/timeOut/AiTimeout.java
@@ -0,0 +1,4 @@
+package com.semillero.Finavex.config.timeOut;
+
+public class AiTimeout {
+}
