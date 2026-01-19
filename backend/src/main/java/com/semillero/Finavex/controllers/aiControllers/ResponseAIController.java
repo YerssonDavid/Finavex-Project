516061dce8f026282f002d9e1d414395a04b3464
David <yerssondavidf@gmail.com>
2026-01-18 20:57:56 -0500

    Response of request AI, directly of Job

--- backend/src/main/java/com/semillero/Finavex/controllers/aiControllers/ResponseAIController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/aiControllers/ResponseAIController.java
@@ -1,4 +1,42 @@
 package com.semillero.Finavex.controllers.aiControllers;
 
+import com.semillero.Finavex.dto.aiDto.ResponseAI;
+import com.semillero.Finavex.services.assistendAI.JobStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping()
+@RequiredArgsConstructor
 public class ResponseAIController {
+    private final JobStore jobStore;
+    @GetMapping("/response/{jobId}")
+    public ResponseEntity<ResponseAI> getResponseAI(@PathVariable String jobId) {
+
+        Object value = jobStore.get(jobId);
+
+        if(value == null){
+            return ResponseEntity.notFound().build();
+        } else if("PENDING".equals(value)){
+            return ResponseEntity.ok().body(new ResponseAI(
+                    "Procesando...",
+                    LocalDateTime.now()
+            ));
+        } else if(value instanceof ResponseAI responseAI){
+            return ResponseEntity.ok().body(responseAI);
+        } else if(value instanceof String error){
+            return ResponseEntity.ok(new ResponseAI(
+                    error,
+                    LocalDateTime.now()
+            ));
+        }
+        return ResponseEntity.internalServerError().build();
+    }
 }
