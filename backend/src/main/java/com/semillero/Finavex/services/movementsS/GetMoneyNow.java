839454808ff90d9438115dfd7e9db094c8b8d2f7
David <yerssondavidf@gmail.com>
2026-01-29 15:56:40 -0500

    Add service for get money now

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMoneyNow.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMoneyNow.java
@@ -1,4 +1,41 @@
 package com.semillero.Finavex.services.movementsS;
 
+import com.semillero.Finavex.dto.movementsMoney.RequestGetMoneyNow;
+import com.semillero.Finavex.dto.movementsMoney.ResponseGetMoneyNow;
+import com.semillero.Finavex.entity.User;
+import com.semillero.Finavex.exceptions.UserNotFoundException;
+import com.semillero.Finavex.repository.UserR;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.text.NumberFormat;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class GetMoneyNow {
+    private final UserR userR;
+
+    public ResponseGetMoneyNow getMoneyNow (RequestGetMoneyNow requestGetMoneyNow){
+        String emailFormat = requestGetMoneyNow.email().toLowerCase().trim();
+
+        if(userR.existsByEmail(emailFormat)){
+            User user = userR.findByEmail(emailFormat).orElseThrow();
+            Double amount = user.getMoneyNow().getCurrentBalance();
+
+            //Format amount
+            NumberFormat format = NumberFormat.getCurrencyInstance();
+            String numberFormat = format.format(amount);
+
+            //Return response
+            ResponseGetMoneyNow response = new ResponseGetMoneyNow (
+                    numberFormat
+            );
+            return response;
+        } else {
+            log.error("El usuario no existe! {}", requestGetMoneyNow.email());
+            throw new UserNotFoundException("El usuario no existe!");
+        }
+    }
 }
