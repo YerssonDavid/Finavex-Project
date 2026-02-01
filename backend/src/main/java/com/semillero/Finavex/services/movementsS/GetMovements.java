b007cdd69e6460a20f86c5a5cf2c7c78d15a7f88
David <yerssondavidf@gmail.com>
2026-02-01 16:13:03 -0500

    Sort for date

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMovements.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMovements.java
@@ -34,7 +34,7 @@ public class GetMovements {
         movements.addAll(expenseR.findMovementsByUserId(userId, time));
         movements.addAll(saveR.findMovementsByUserId(userId, time));
 
-        movements.sort(Comparator.comparing(ResponseGetMovements::date));
+        movements.sort(Comparator.comparing(ResponseGetMovements::date).reversed());
 
         return movements;
     }
