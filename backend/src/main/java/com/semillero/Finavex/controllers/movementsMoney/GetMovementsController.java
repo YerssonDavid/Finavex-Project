6a0f8ca5c3775c70c66250fcead8be2951ea823e
David <yerssondavidf@gmail.com>
2026-02-01 15:21:09 -0500

    Update GetMovementsController endpoint response handling

--- backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/GetMovementsController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/GetMovementsController.java
@@ -1,4 +1,27 @@
 package com.semillero.Finavex.controllers.movementsMoney;
 
-public class GetMovements {
+import com.semillero.Finavex.dto.movementsMoney.RequestGetMovements;
+import com.semillero.Finavex.dto.movementsMoney.ResponseGetMovements;
+import com.semillero.Finavex.services.movementsS.GetMovements;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movements")
+@RequiredArgsConstructor
+public class GetMovementsController {
+    private final GetMovements getMovements;
+
+    @PostMapping
+    public ResponseEntity<List<ResponseGetMovements>> getMovements(@Valid @RequestBody RequestGetMovements requestGetMovements){
+        List<ResponseGetMovements> movements = getMovements.getMovementsUser(requestGetMovements);
+        return ResponseEntity.ok(movements);
+    }
 }
