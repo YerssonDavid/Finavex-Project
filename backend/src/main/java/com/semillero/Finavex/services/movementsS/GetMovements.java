9cebad4948d7c7834dab85be2809d162b0cda6d3
David <yerssondavidf@gmail.com>
2026-02-01 15:20:58 -0500

    Update service layer for movement operations

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMovements.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/GetMovements.java
@@ -1,4 +1,38 @@
 package com.semillero.Finavex.services.movementsS;
 
+import com.semillero.Finavex.dto.movementsMoney.RequestGetMovements;
+import com.semillero.Finavex.dto.movementsMoney.ResponseGetMovements;
+import com.semillero.Finavex.exceptions.UserNotFoundException;
+import com.semillero.Finavex.repository.UserR;
+import com.semillero.Finavex.repository.movementsR.ExpenseR;
+import com.semillero.Finavex.repository.movementsR.SaveR;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class GetMovements {
+    private final ExpenseR expenseR;
+    private final SaveR saveR;
+    private final UserR userR;
+
+
+    public List<ResponseGetMovements> getMovementsUser (RequestGetMovements requestGetMovements){
+        String emailFormat = requestGetMovements.email().toLowerCase().trim();
+        if(!userR.existsByEmail(emailFormat)){
+            throw new UserNotFoundException("Usuario no encontrado!");
+        }
+
+        LocalDateTime time = LocalDateTime.now().minusDays(30);
+        List<ResponseGetMovements> movements = new ArrayList<>();
+        Long userId = userR.findByEmail(emailFormat).get().getId();
+        movements.addAll(expenseR.findMovementsByUserId(userId, time));
+        movements.addAll(saveR.findMovementsByUserId(userId, time));
+
+        return movements;
+    }
 }
