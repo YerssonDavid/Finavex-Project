1b05cf0f29721eb11b80ea3951782f5295f760ca
David <yerssondavidf@gmail.com>
2026-02-02 18:47:00 -0500

    Add documentation of API

--- backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/SumTotalSaveMonthController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/SumTotalSaveMonthController.java
@@ -3,6 +3,8 @@ package com.semillero.Finavex.controllers.movementsMoney;
 import com.semillero.Finavex.dto.movementsMoney.RequestSumTotalSaveMonth;
 import com.semillero.Finavex.dto.movementsMoney.ResponseSumTotalSaveMonth;
 import com.semillero.Finavex.services.movementsS.SumTotalSaveMonth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +17,20 @@ public class SumTotalSaveMonthController {
     private final SumTotalSaveMonth sumTotalSaveMonth;
 
     @PostMapping()
+    @Operation(
+            summary = "Sum total of savings for the month",
+            description = "Endpoint for summing the total savings of a user in the current month",
+            method = "POST",
+            tags = {"Sum total"},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "It is expected email",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = String.class)
+                    ),
+                    required = true
+            )
+    )
     public ResponseEntity<ResponseSumTotalSaveMonth> sumTotal(@RequestBody @Validated RequestSumTotalSaveMonth emailRequest){
         ResponseSumTotalSaveMonth response = sumTotalSaveMonth.sumTotalSaveMonth(emailRequest.email().toLowerCase().trim());
         return ResponseEntity.ok(response);
