aa069af2ce114e70cfba52c51a640d190cb5b29e
David <yerssondavidf@gmail.com>
2026-02-06 19:24:24 -0500

    Added entity of savings users

--- backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsMoneyUsers.java
+++ backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsMoneyUsers.java
@@ -1,4 +1,38 @@
 package com.semillero.Finavex.entity.movements;
 
-public class SavingsMoneyUser {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name="savings_money_users")
+public class SavingsMoneyUsers {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @Email
+    @NotBlank(message = "El email se requiere")
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @NotBlank(message = "El nombre se requiere")
+    @Column(name="name_savings", nullable = false)
+    private String nameSavings;
+
+    @Positive
+    @DecimalMin("0.01")
+    @NotNull(message = "El monto no puede ser nulo")
+    @Column(name="amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name="date_of_registry", nullable = false)
+    private LocalDateTime dateOfRegistry;
+
 }
