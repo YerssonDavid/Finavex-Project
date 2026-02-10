c3955bef753c6caf3c9e2a3d4dea09ebe2bbe44b
David <yerssondavidf@gmail.com>
2026-02-09 22:11:26 -0500

    Added query for get sum savings user

--- backend/src/main/java/com/semillero/Finavex/repository/movementsR/SavingsUser.java
+++ backend/src/main/java/com/semillero/Finavex/repository/movementsR/SavingsUser.java
@@ -2,10 +2,19 @@ package com.semillero.Finavex.repository.movementsR;
 
 import com.semillero.Finavex.entity.movements.SavingsMoneyUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 
 @Repository
 public interface SavingsUser extends JpaRepository<SavingsMoneyUsers, Long> {
+    boolean existsById(Long id);
+
+    @Query("""
+            SELECT COALESCE(SUM(s.amount), 0)
+            FROM SavingsMoneyUsers s
+            WHERE s.user.id = (SELECT u.id FROM User u WHERE u.email = :email)
+        """)
+    BigDecimal sumTotalSavingsByEmail(String email);
 }
