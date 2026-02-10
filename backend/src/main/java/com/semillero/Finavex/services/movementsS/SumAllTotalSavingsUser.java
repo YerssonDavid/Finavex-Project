12a68909d1f9e91d0a0ccdb0a5d1198022db0ad7
David <yerssondavidf@gmail.com>
2026-02-09 22:12:20 -0500

    Add service for get sum total savings user

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/SumAllTotalSavingsUser.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/SumAllTotalSavingsUser.java
@@ -1,4 +1,35 @@
 package com.semillero.Finavex.services.movementsS;
 
-public class SumTotalSavingsUser {
+import com.semillero.Finavex.dto.movementsMoney.ResponseSumAllTotalSavingsUser;
+import com.semillero.Finavex.exceptions.UserNotFoundException;
+import com.semillero.Finavex.repository.UserR;
+import com.semillero.Finavex.repository.movementsR.SavingsUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class SumAllTotalSavingsUser {
+    private final SavingsUser savingsUser;
+    private final UserR userR;
+
+    public ResponseSumAllTotalSavingsUser sumtotal(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        String emailFormat = email.toLowerCase().trim();
+
+        Long idUser = userR.getIdByEmail(emailFormat);
+
+        if(!savingsUser.existsById(idUser)){
+            throw new UserNotFoundException("El usuario no existe!");
+        }
+
+        BigDecimal sumTotalUser = savingsUser.sumTotalSavingsByEmail(emailFormat);
+        if(sumTotalUser.compareTo(BigDecimal.ZERO)< 0 || sumTotalUser.compareTo(BigDecimal.ZERO) == 0){
+            return new ResponseSumAllTotalSavingsUser(BigDecimal.ZERO);
+        }
+        return new ResponseSumAllTotalSavingsUser(sumTotalUser);
+    }
 }
