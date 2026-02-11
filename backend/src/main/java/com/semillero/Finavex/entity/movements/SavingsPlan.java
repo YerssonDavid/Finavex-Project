f6bfc76bbe2d2d618860211cd1892c1e87e8b0cf
David <yerssondavidf@gmail.com>
2026-02-10 20:33:25 -0500

    Added entity for registry plans of savings

--- backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsPlan.java
+++ backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsPlan.java
@@ -1,4 +1,33 @@
 package com.semillero.Finavex.entity.movements;
 
+import com.semillero.Finavex.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="savings_plan")
+@Data
 public class SavingsPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message="Se requiere un nombre para el plan de ahorro")
+    @Column(name="name_plan", nullable = false)
+    private String nameSavingsPlan;
+
+    @NotNull(message = "Se requiere un valor para la meta de ahorro")
+    // Value minimum of $1.000 for meta plan
+    @DecimalMin("1000.00")
+    @Column(name="meta_plan", nullable = false)
+    private BigDecimal amountMetaPlan;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User userId;
 }
