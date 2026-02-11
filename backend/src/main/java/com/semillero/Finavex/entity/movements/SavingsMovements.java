d793aa641ac36fcf3053e0f8a79c9ef0d7fc449b
David <yerssondavidf@gmail.com>
2026-02-10 20:32:13 -0500

    Added entity for registry savings movements of users

--- backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsMovements.java
+++ backend/src/main/java/com/semillero/Finavex/entity/movements/SavingsMovements.java
@@ -1,4 +1,31 @@
 package com.semillero.Finavex.entity.movements;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="savings_movements")
+@Data
 public class SavingsMovements {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Positive
+    @DecimalMin("0.01")
+    @Column(name="amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name="date", nullable = false)
+    private LocalDateTime dateRegistry;
+
+    // Relation directly with the savings plan
+    @ManyToOne
+    @JoinColumn(name = "savings-plan", nullable = false)
+    private SavingsPlan savingsPlan;
 }
