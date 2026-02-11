702540f25e743966731ba8b66485189becde4b3a
David <yerssondavidf@gmail.com>
2026-02-10 20:35:22 -0500

    Added repository of savings plan with query for get the object complete

--- backend/src/main/java/com/semillero/Finavex/repository/movementsR/SavingsPlanR.java
+++ backend/src/main/java/com/semillero/Finavex/repository/movementsR/SavingsPlanR.java
@@ -1,4 +1,15 @@
 package com.semillero.Finavex.repository.movementsR;
 
-public interface SavingsPlanR {
+import com.semillero.Finavex.entity.movements.SavingsPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SavingsPlanR extends JpaRepository <SavingsPlan, Long> {
+    @Query("""
+            SELECT sp
+            FROM SavingsPlan sp
+            JOIN sp.userId u
+            WHERE u.id = :id
+""")
+    SavingsPlan getSavingsPlanByUserId (Long id);
 }
