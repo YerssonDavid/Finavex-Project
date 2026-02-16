cd69037b8350b05c9624e35c85613b606e93e7f1
David <yerssondavidf@gmail.com>
2026-02-15 21:53:56 -0500

    Add description field to RequestRegistrySavingsPlan DTO

--- backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/RequestRegistrySavingsPlan.java
+++ backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/RequestRegistrySavingsPlan.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 public record RequestRegistrySavingsPlan (
         String nameSavingsPlan,
-        BigDecimal amountMetaPlan
+        BigDecimal amountMetaPlan,
+        String descriptionPlanSavings
 ){
 }
