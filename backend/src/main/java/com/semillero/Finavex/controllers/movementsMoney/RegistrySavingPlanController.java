09808a3b702614a07ba6d2ff78bef7d80da51466
David <yerssondavidf@gmail.com>
2026-02-17 21:17:03 -0500

    Add endpoint to list registry savings plans

--- backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/RegistrySavingPlanController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/RegistrySavingPlanController.java
@@ -1,24 +1,31 @@
 package com.semillero.Finavex.controllers.movementsMoney;
 
 import com.semillero.Finavex.dto.movementsMoney.RequestRegistrySavingsPlan;
+import com.semillero.Finavex.dto.movementsMoney.ResponseListRegistrySavingsPlan;
 import com.semillero.Finavex.dto.movementsMoney.ResponseRegistrySavingsPlan;
 import com.semillero.Finavex.services.movementsS.SavingsPlanRegistry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/registry/saving-plan")
+@RequestMapping()
 @RequiredArgsConstructor
 public class RegistrySavingPlanController {
     private final SavingsPlanRegistry savingsPlanRegistry;
 
-    @PostMapping
+    @PostMapping("/registry/saving-plan")
     public ResponseEntity<ResponseRegistrySavingsPlan> registrySavingPlan (@RequestBody RequestRegistrySavingsPlan requestRegistrySavingsPlan){
         ResponseRegistrySavingsPlan response = savingsPlanRegistry.registrySavingPlan(requestRegistrySavingsPlan);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/list/registry/saving-plan")
+    public ResponseEntity<List<ResponseListRegistrySavingsPlan>> listRegistrySavingPlan (){
+        List<ResponseListRegistrySavingsPlan> response = savingsPlanRegistry.listRegistrySavingsPlan();
+        return ResponseEntity.status(200).body(response);
     }
 }
---
+++ backend/src/main/java/com/semillero/Finavex/exceptions/PlanNotExist.java
@@ -0,0 +1,7 @@
+package com.semillero.Finavex.exceptions;
+
+public class PlanNotExist extends RuntimeException {
+  public PlanNotExist(String message) {
+    super(message);
+  }
+}
