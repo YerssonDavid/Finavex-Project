034f9c1f1996fdff0f006e611e61767cb4f7346d
David <yerssondavidf@gmail.com>
2026-02-19 20:08:41 -0500

    Added custom exception

--- backend/src/main/java/com/semillero/Finavex/exceptions/ExistElement.java
+++ backend/src/main/java/com/semillero/Finavex/exceptions/ExistElement.java
@@ -1,7 +1,11 @@
 package com.semillero.Finavex.exceptions;
 
 public class ExistElement extends RuntimeException {
-  public ExistElement(String message) {
-    super(message);
-  }
+    public ExistElement(String message) {
+        super(message);
+    }
+
+    public ExistElement(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
