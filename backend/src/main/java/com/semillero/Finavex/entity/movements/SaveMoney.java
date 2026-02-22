0615fc49efda2068c3037280252742cd85a89f29
David <yerssondavidf@gmail.com>
2026-02-21 20:04:22 -0500

    Change the name of table and columns

--- backend/src/main/java/com/semillero/Finavex/entity/movements/SaveMoney.java
+++ backend/src/main/java/com/semillero/Finavex/entity/movements/SaveMoney.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="Ahorros")
+@Table(name="Savings")
 @Getter
 @Setter
 public class SaveMoney {
@@ -18,19 +18,19 @@ public class SaveMoney {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="tipo-movimiento", nullable = false)
+    @Column(name="movement_type", nullable = false)
     private String movementType;
 
-    @Column(name="valor-ahorrado", nullable = false)
+    @Column(name="savings_value", nullable = false)
     private BigDecimal amount;
 
-    @Column(name="fecha-movimento-ingreso", nullable= false, length = 10)
+    @Column(name="date_movement_income", nullable= false, length = 10)
     private LocalDateTime date;
 
-    @Column(name="nota-movimiento", nullable=true, length = 100)
+    @Column(name="movement_note", nullable=true, length = 100)
     private String noteMovement;
 
     @ManyToOne
-    @JoinColumn(name="id-user", nullable = false, unique = false)
+    @JoinColumn(name="id_user", nullable = false, unique = false)
     private User user;
 }
