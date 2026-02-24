de5aa549270f75c652f1ec41a1592d63e0b2f777
David <yerssondavidf@gmail.com>
2026-02-24 17:04:28 -0500

    Added repository of movements in savings

---
+++ backend/src/main/java/com/semillero/Finavex/repository/movementsR/SavingsMovementsR.java
@@ -0,0 +1,8 @@
+package com.semillero.Finavex.repository.movementsR;
+
+import com.semillero.Finavex.entity.movements.SavingsMovements;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SavingsMovementsR extends JpaRepository<SavingsMovements, Long> {
+
+}
