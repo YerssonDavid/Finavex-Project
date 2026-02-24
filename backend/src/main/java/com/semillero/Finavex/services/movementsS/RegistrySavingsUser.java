56b04ca3d489be87e117a7497e21b8b2dea33e31
David <yerssondavidf@gmail.com>
2026-02-24 17:11:08 -0500

    Added new instance of SavingsMoneyUsers for registry the movements

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/RegistrySavingsUser.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/RegistrySavingsUser.java
@@ -1,16 +1,17 @@
 package com.semillero.Finavex.services.movementsS;
 
-import com.semillero.Finavex.dto.exception.ErrorGeneral;
 import com.semillero.Finavex.dto.movementsMoney.RequestRegistrySavingsUser;
 import com.semillero.Finavex.dto.movementsMoney.ResponseRegistrySavingsUser;
 import com.semillero.Finavex.entity.User;
+import com.semillero.Finavex.entity.movements.SavingsMoneyUsers;
 import com.semillero.Finavex.entity.movements.SavingsMovements;
 import com.semillero.Finavex.entity.movements.SavingsPlan;
 import com.semillero.Finavex.exceptions.PlanNotExist;
 import com.semillero.Finavex.exceptions.UserNotFoundException;
 import com.semillero.Finavex.repository.UserR;
+import com.semillero.Finavex.repository.movementsR.SavingsMovementsR;
 import com.semillero.Finavex.repository.movementsR.SavingsPlanR;
-import com.semillero.Finavex.repository.movementsR.SavingsUserR;
+import com.semillero.Finavex.repository.movementsR.SavingsMoneyUserR;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,13 +20,13 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class RegistrySavingsUser {
-    private final SavingsUserR savingUser;
+    private final SavingsMoneyUserR savingUser;
     private final SavingsPlanR savingsPlanR;
+    private final SavingsMovementsR savingsMovementsR;
     private final UserR userR;
 
     public ResponseRegistrySavingsUser registrySavingsUser(RequestRegistrySavingsUser request){
@@ -55,11 +56,20 @@ public class RegistrySavingsUser {
                 .findFirst()
                 .orElseThrow(() -> new PlanNotExist("El plan de ahorro no existe o no pertenece al usuario!"));
 
+        // Save in table of Savings Movements
         SavingsMovements savingsMovements = new SavingsMovements();
         savingsMovements.setAmount(request.amount());
         savingsMovements.setDateRegistry(LocalDateTime.now());
         savingsMovements.setSavingsPlan(savingsPlan);
-        savingUser.save(savingsMovements);
+        savingsMovementsR.save(savingsMovements);
+
+        //Save in table de money user
+        SavingsMoneyUsers savingsMoneyUsers = new SavingsMoneyUsers();
+        savingsMoneyUsers.setUser(user);
+        savingsMoneyUsers.setNameSavings(savingsMovements.getSavingsPlan().getNameSavingsPlan());
+        savingsMoneyUsers.setAmount(request.amount());
+        savingsMoneyUsers.setDateOfRegistry(LocalDateTime.now());
+        savingUser.save(savingsMoneyUsers);
 
         return new ResponseRegistrySavingsUser(true);
     }
