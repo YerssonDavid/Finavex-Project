8ea9a517577c36add9ddb0ae774e8fca3e1b3e0f
David <yerssondavidf@gmail.com>
2026-02-25 20:09:38 -0500

    Refactor SumAllTotalSavingsUser service to use DataService

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/SumAllTotalSavingsUser.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/SumAllTotalSavingsUser.java
@@ -1,5 +1,6 @@
 package com.semillero.Finavex.services.movementsS;
 
+import com.semillero.Finavex.dto.DataService;
 import com.semillero.Finavex.dto.movementsMoney.ResponseSumAllTotalSavingsUser;
 import com.semillero.Finavex.exceptions.UserNotFoundException;
 import com.semillero.Finavex.repository.UserR;
@@ -10,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,19 +20,16 @@ public class SumAllTotalSavingsUser {
     private final SavingsMoneyUserR savingsUser;
     private final UserR userR;
 
-    public ResponseSumAllTotalSavingsUser sumtotal(){
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        String emailFormat = email.toLowerCase().trim();
-
-        Long idUser = userR.getIdByEmail(emailFormat);
+    public ResponseSumAllTotalSavingsUser sumtotal(DataService dataService){
+        Optional<String> email = dataService.email();
+        Long idUser = userR.getIdByEmail(email.orElse("Error al obtener email del usuario"));
         log.warn("Id del usuario {}", idUser);
-
 
         if(!savingsUser.existsById(idUser)){
             throw new UserNotFoundException("El usuario no existe!");
         }
 
-        BigDecimal sumTotalUser = savingsUser.sumTotalSavingsByEmail(emailFormat);
+        BigDecimal sumTotalUser = savingsUser.sumTotalSavingsByEmail(email.orElse("Error al obtener email del usuario"));
         if(sumTotalUser.compareTo(BigDecimal.ZERO)< 0 || sumTotalUser.compareTo(BigDecimal.ZERO) == 0){
             return new ResponseSumAllTotalSavingsUser(BigDecimal.ZERO);
         }
