446a711c71c024aae34fc28d054c90550e6f5b5a
David <yerssondavidf@gmail.com>
2026-02-25 20:10:01 -0500

    Refactor SumTotalExpenseMonth service to use DataService

--- backend/src/main/java/com/semillero/Finavex/services/movementsS/SumTotalExpenseMonth.java
+++ backend/src/main/java/com/semillero/Finavex/services/movementsS/SumTotalExpenseMonth.java
@@ -1,25 +1,25 @@
 package com.semillero.Finavex.services.movementsS;
 
+import com.semillero.Finavex.dto.DataService;
 import com.semillero.Finavex.dto.movementsMoney.ResponseSumTotalExpensesMonth;
 import com.semillero.Finavex.repository.movementsR.ExpenseR;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SumTotalExpenseMonth {
     private final ExpenseR expenseR;
 
-    public ResponseSumTotalExpensesMonth sumTotalExpenseMonth() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        String emailFormat = email != null ? email.toLowerCase().trim() : null;
+    public ResponseSumTotalExpensesMonth sumTotalExpenseMonth(DataService dataService) {
+        Optional<String> email = dataService.email();
 
-        if(emailFormat == null || expenseR.idByEmail(emailFormat) == null){
+        if(email.isEmpty() || expenseR.idByEmail(email.orElse("No se pudo obtener el email!")) == null){
             throw new IllegalArgumentException();
         }
 
@@ -31,7 +31,7 @@ public class SumTotalExpenseMonth {
 
         LocalDateTime end = start.plusMonths(1);
 
-        Double sum = expenseR.sumByPeriod(start, end, emailFormat);
+        Double sum = expenseR.sumByPeriod(start, end, email.orElse("No se pudo obtener el email!"));
 
         NumberFormat format = NumberFormat.getNumberInstance();
         String sumFormat = format.format(sum);
