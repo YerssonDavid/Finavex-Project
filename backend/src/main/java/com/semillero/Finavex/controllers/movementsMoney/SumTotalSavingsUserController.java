aa276b20837ca0ed10a3056fd42faae90c2e2130
David <yerssondavidf@gmail.com>
2026-02-25 19:57:38 -0500

    Refactor and Get authentication in controller and send the data to service in DTO Data Service

--- backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/SumTotalSavingsUserController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/SumTotalSavingsUserController.java
@@ -1,12 +1,17 @@
 package com.semillero.Finavex.controllers.movementsMoney;
 
+import com.semillero.Finavex.dto.DataService;
 import com.semillero.Finavex.dto.movementsMoney.ResponseSumAllTotalSavingsUser;
 import com.semillero.Finavex.services.movementsS.SumAllTotalSavingsUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sumTotal/savings")
@@ -16,7 +21,12 @@ public class SumTotalSavingsUserController {
 
     @GetMapping()
     public ResponseEntity<ResponseSumAllTotalSavingsUser> sumAllTotalSavingsUser (){
-        ResponseSumAllTotalSavingsUser response = sumAllTotalSavingsUser.sumtotal();
+        Optional<String> email = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getName)
+                .filter(name -> !name.trim().isEmpty())
+                .map(name -> name.toLowerCase().trim());
+
+        ResponseSumAllTotalSavingsUser response = sumAllTotalSavingsUser.sumtotal(DataService.builder().email(email).build());
         return ResponseEntity.ok(response);
     }
 }
