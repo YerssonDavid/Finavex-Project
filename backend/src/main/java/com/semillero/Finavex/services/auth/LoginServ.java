cc93488f27ff7feb0b7e321ffc1dd59dedacb9b4
David <yerssondavidf@gmail.com>
2026-03-09 17:39:20 -0500

    Change the route of the service

--- backend/src/main/java/com/semillero/Finavex/services/auth/LoginServ.java
+++ backend/src/main/java/com/semillero/Finavex/services/auth/LoginServ.java
@@ -1,4 +1,4 @@
-package com.semillero.Finavex.services.login;
+package com.semillero.Finavex.services.auth;
 
 import com.semillero.Finavex.config.security.Security;
 import com.semillero.Finavex.dto.ApiResponse;
@@ -15,7 +15,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
