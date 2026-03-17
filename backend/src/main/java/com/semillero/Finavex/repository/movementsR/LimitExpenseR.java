2e98b3c60d2985c62654ab9019c0e4b029cc1c09
David <yerssondavidf@gmail.com>
2026-03-16 19:45:47 -0500

    Added repository of the table registry limit expenses

--- backend/src/main/java/com/semillero/Finavex/repository/movementsR/LimitExpenseR.java
+++ backend/src/main/java/com/semillero/Finavex/repository/movementsR/LimitExpenseR.java
@@ -1,4 +1,8 @@
 package com.semillero.Finavex.repository.movementsR;
 
-public interface LimitExpenseR {
+import com.semillero.Finavex.entity.movements.LimitExpense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LimitExpenseR extends JpaRepository<LimitExpense, Long> {
+
 }
