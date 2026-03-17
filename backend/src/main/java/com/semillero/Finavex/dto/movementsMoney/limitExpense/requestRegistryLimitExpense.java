94816b168e9f790622049ecc54e4651777d7bd20
David <yerssondavidf@gmail.com>
2026-03-16 19:43:29 -0500

    Added DTO for the request of the service registry limit of expense

--- backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/limitExpense/requestRegistryLimitExpense.java
+++ backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/limitExpense/requestRegistryLimitExpense.java
@@ -1,4 +1,18 @@
 package com.semillero.Finavex.dto.movementsMoney.limitExpense;
 
-public class requestRegistyLimitExpense {
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public record requestRegistryLimitExpense (
+        @NotNull(message="El valor no puede estar vacio")
+        @DecimalMin(value="100.00", message="El valor debe ser mayor a $100.00")
+        BigDecimal valueLimit,
+
+        @NotNull(message="Se debe de establecer una fecha de expiracion para el limite")
+        Date expirationDateRegistry
+){
 }
