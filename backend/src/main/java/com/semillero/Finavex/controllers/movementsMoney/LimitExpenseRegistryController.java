29e889396336abc70b734893d0b0c26ff6caaa75
David <yerssondavidf@gmail.com>
2026-03-16 20:29:16 -0500

    Implement REST endpoint for limit expense registration

--- backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/LimitExpenseRegistryController.java
+++ backend/src/main/java/com/semillero/Finavex/controllers/movementsMoney/LimitExpenseRegistryController.java
@@ -1,10 +1,11 @@
 package com.semillero.Finavex.controllers.movementsMoney;
 
 import com.semillero.Finavex.dto.ApiResponse;
-import com.semillero.Finavex.dto.movementsMoney.limitExpense.requestRegistryLimitExpense;
+import com.semillero.Finavex.dto.movementsMoney.limitExpense.RequestRegistryLimitExpense;
 import com.semillero.Finavex.services.movementsS.limitExpense.LimitExpenseRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,9 @@ public class LimitExpenseRegistryController {
     private final LimitExpenseRegistry limitExpenseRegistry;
 
     @PostMapping()
-    public ResponseEntity<ApiResponse> registryLimitExpenseC (@RequestBody requestRegistryLimitExpense request){
-        ApiResponse response = limitExpenseRegistry.registerLimitExpense(request);
+    public ResponseEntity<ApiResponse> registryLimitExpenseC (@RequestBody RequestRegistryLimitExpense request){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName().toLowerCase().trim();
+        ApiResponse response = limitExpenseRegistry.registerLimitExpense(request, email);
         return ResponseEntity.ok(response);
     }
 }
