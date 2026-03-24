0b9d8f2af062b051bcc0c5acf39211b3a966ce6f
David <yerssondavidf@gmail.com>
2026-03-23 20:31:04 -0500

    Add response data structure for limit expense registry

--- backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/limitExpense/ResponseDataRegistryDataBase.java
+++ backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/limitExpense/ResponseDataRegistryDataBase.java
@@ -1,4 +1,12 @@
 package com.semillero.Finavex.dto.movementsMoney.limitExpense;
 
-public record ResponseDataRegistryDataBase() {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public record ResponseDataRegistryDataBase(
+        BigDecimal value,
+        Date expiration,
+        LocalDateTime dateRegistry
+) {
 }
