9a5221a9f1193d785a022f5c91677088c10d2d97
David <yerssondavidf@gmail.com>
2026-03-23 19:09:55 -0500

    Update savings plans response DTO structure

--- backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/ResponseGetPlansSavings.java
+++ backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/ResponseGetPlansSavings.java
@@ -1,8 +1,6 @@
 package com.semillero.Finavex.dto.movementsMoney;
 
-import com.semillero.Finavex.entity.movements.SavingsPlan;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public record ResponseGetPlansSavings(
