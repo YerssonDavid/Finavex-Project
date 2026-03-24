8aaff78b10bdc37c325aed587e5426c89b94bb50
David <yerssondavidf@gmail.com>
2026-03-23 19:10:00 -0500

    Update registry savings plan list response DTO

--- backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/ResponseListRegistrySavingsPlan.java
+++ backend/src/main/java/com/semillero/Finavex/dto/movementsMoney/ResponseListRegistrySavingsPlan.java
@@ -1,9 +1,6 @@
 package com.semillero.Finavex.dto.movementsMoney;
 
-import com.semillero.Finavex.entity.movements.SavingsPlan;
 
-import java.util.HashMap;
-import java.util.List;
 
 public record ResponseListRegistrySavingsPlan(
         Long id,
